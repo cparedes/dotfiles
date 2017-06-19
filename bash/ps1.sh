6085d11fba3beb73ce6129ca8a609434edfc6f1c
Ian Paredes <iparedes@grubhub.com>
2017-06-19 16:10:18 -0400

    Greatly slim down dotfiles. Hasn't been updated in over 6 years, so figured I'll update it with less elaborate crap. Who needs colors and custom PS1 variables?!

--- bash/ps1.sh
+++
@@ -1,1 +0,0 @@
-PS1="\$(return_code_face) \W :: ${GREEN}\u${WHITE}:${GREEN}\h${WHITE} ${YELLOW}\$(parse_git_branch)\$(parse_svn_branch)${WHITE}\n[\t] => \[\e[0m\]"
--- bash/returncode.sh
+++
@@ -1,7 +0,0 @@
-return_code_face() {
-  if [ $? = 0 ]; then
-    echo ^_^
-  else
-    echo O_O
-  fi
-}
